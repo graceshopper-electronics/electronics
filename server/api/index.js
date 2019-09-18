66c7bc2829602c9af0e18233efddf06017cd65ac
Christopher Choi <christopherchoi97@gmail.com>
2019-09-18 10:16:12 -0500

    separated order routes to carts and history

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,9 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
-router.use('/orders', require('./orders'))
+router.use('/orders/history', require('./orderHistory'))
+
+router.use('orders/cart', require('./cart'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
