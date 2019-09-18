933f1140f50023e5298d0a5a36f6863bb908e6f7
Christopher Choi <christopherchoi97@gmail.com>
2019-09-18 09:26:45 -0500

    created navbar

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,8 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
+router.use('/addToCart', require('./addToCart'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
