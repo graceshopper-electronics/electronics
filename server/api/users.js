df6862c9ac5b7e0f8280b14241b84830f0304138
Andre Birkus <birkusandre@gmail.com>
2019-09-19 16:52:28 -0500

    adding admin routes

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,36 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+const onlyAdmins = (req, res, next) => {
+  if (!req.user) {
+    console.log('USER IS NOT LOGGED IN!')
+    return res.sendStatus(401)
+  }
+  if (!req.user.isAdmin) {
+    console.log('USER LOGEED IN BUT NOT AN ADMIN!')
+    return res.sendStatus(401)
+  }
+  next()
+}
+
+router.param('id', (req, res, next, id) => {
+  User.findById(id)
+    .then(user => {
+      if (!user) throw HttpError(404)
+      req.requestedUser = user
+      next()
+      return null
+    })
+    .catch(next)
+})
+
+router.delete('/:id', onlyAdmins, async (req, res, next) => {
+  try {
+    const user = req.requestedUser
+    await user.destroy()
+    res.status(204)
+  } catch (err) {
+    next(err)
+  }
+})
